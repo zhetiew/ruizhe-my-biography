dde0f090675205acd39345c8d38731fb15be11d1
zhetiew <zhetiew@gmail.com>
2021-10-24 07:23:18 +0000

    setting up

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import vercel from '@sveltejs/adapter-vercel';
+
+const config = {
+    kit: {
+   	 // hydrate the <div id="svelte"> element in src/app.html
+   	 target: '#svelte',
+   	 adapter: vercel()
+    }
+};
+
+export default config;
