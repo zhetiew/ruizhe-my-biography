9b35a35e6481d3d7694dca7d3e91f46c475d6c83
zhetiew <zhetiew@gmail.com>
2021-10-31 05:44:14 +0000

    changes2

---
+++ src/lib/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let color;
+</script>
+
+<div style="background:{color}">
+	<p>Color : {color}</p>
+</div>
+
+<style>
+	div {
+		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
+		padding: 0.5em;
+		width: 120px;
+		margin: 0.5em auto;
+		border-radius: 5px;
+		text-align: center;
+		font-weight: bold;
+	}
+	p {
+		margin: 0;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Color from '$lib/Color.svelte';
+    let colorName = 'Pink';
+</script>
+
+<div>
+    <h1 class="text-primary">Play with Colors</h1>
+</div>
+
+<div>
+    <input bind:value={colorName} />
+    <Color color={colorName} />
+</div>
+
+<style>
+    input {
+   	 padding: 0.5em;
+   	 text-align: center;
+    }
+    div {
+   	 text-align: center;
+   	 margin: 3em auto;
+    }
+</style>
