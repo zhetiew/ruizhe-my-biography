83cd1d333db852086503e531e7b73220d4e794c4
zhetiew <zhetiew@gmail.com>
2021-11-07 06:49:26 +0000

    changes again

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,21 +3,3 @@
 </script>
 
 <h1>{title}</h1>
-<div style="background:{color}">
-	<p>Color : {color}</p>
-</div>
-
-<style>
-	div {
-		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
-		padding: 0.5em;
-		width: 120px;
-		margin: 0.5em auto;
-		border-radius: 5px;
-		text-align: center;
-		font-weight: bold;
-	}
-	p {
-		margin: 0;
-	}
-</style>
