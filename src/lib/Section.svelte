88b272f8900c6fc57f19cfbaa1f9a13b118fdf5c
zhetiew <zhetiew@gmail.com>
2021-11-07 07:23:28 +0000

    added info

---
+++ src/lib/Section.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let title;
+    export let item1;
+    export let item2;
+    export let item3;
+</script>
+
+<div>
+    <h3>{title}</h3>
+    <p>{item1}</p>
+    <p>{item2}</p>
+    <p>{item3}</p>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,26 @@
 <script>
     import Header from '$lib/Header.svelte';
+    import Section from '$lib/Section.svelte'
+    let myInfo = {
+    title: "Info",
+    item1: "Tiew Rui Zhe",
+    item2: 13,
+    item3: "SMJK (C) Chung Ling",
+    }
+    let myHobby = {
+    title: "Hobby",
+    item1: "Reading",
+    item2: "Playing games",
+    item3: "Coding",
+    }
+    let myPersonality = {
+    title: "Personality",
+    item1: "Quiet",
+    item2: "Not handsome like Ernest",
+    item3: "Humble",
+    }
 </script>
 <Header title={"My Biography"}/>
+<Section {...myInfo}/>
+<Section {...myHobby}/>
+<Section {...myPersonality}/>
